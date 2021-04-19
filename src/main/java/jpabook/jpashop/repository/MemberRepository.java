4f0cc9ff8274defbfa121e5b4380eddb86e4a97f
coolpixer <73987782+coolpixer@users.noreply.github.com>
2021-04-19 10:02:50 +0900

    도메인 작성

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    // boot가 EntityManager의 인스턴스를 주입해준다.
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @DisplayName("멤버 정보를 등록했을 때 등록된 멤버의 ID가 조회시 ID와 같다")
+    @Rollback(false)
+    void save() {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        final Long id = memberRepository.save(member);
+        Member memberSaved = memberRepository.find(id);
+
+        //then
+        assertThat(memberSaved.getId()).isEqualTo(member.getId());
+        assertThat(memberSaved.getUsername()).isEqualTo(member.getUsername());
+    }
+
+    @Test
+    void find() {
+        assertThat(1).isEqualTo(1);
+    }
+}
